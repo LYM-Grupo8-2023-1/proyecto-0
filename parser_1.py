905993f7189f8aecbedc5570dc4b824bfc242822
Mariana Lozano <m.lozanor2@uniandes.edu.co>
2023-02-09 09:39:28 -0500

    .

--- parser_1.py
+++ parser_1.py
@@ -4,7 +4,7 @@ from inspect import currentframe, getframeinfo
 
 #definicion de componentes
 comandos = ["M","R","C","B","c","b","P","J","G"] 
-instrucciones = ["assignTo", "goto", "move", "turn", "face", "put", "pick", "moveToThe", "moveInDir", "jumpToThe", "jumpInDir", "nop", "PROCS"]
+instrucciones = ["assignTo", "goto", "move", "turn", "face", "put", "pick", "moveToThe", "moveInDir", "jumpToThe", "jumpInDir", "nop", "ROBOT_R"]
 condiciones = ["facing", "canPut", "canPick","canMoveInDir", "canJumpInDir", "canMoveToThe", "canJumpToThe", "not"]
 funcionesCondicionales = ["while", "do", "if", "od", "fi", "else"]
 direcciones = ["left","right","front","back"]
@@ -15,10 +15,22 @@ def main():
     tokens = tk.word_tokenize(file,"",True)
     parser(tokens)
 
+def identificadorDeSyntax(palabra, lista):
+    count=0
+    for each in lista:
+        if palabra == each:
+            count+=1
+
+    if count == 1:
+        return True
+    else:
+        return False
+
 
 def syntaxError(frameinfo):
     print("Error de sintaxis detectado: ", frameinfo.lineno)
     exit()
 
 
-#def parser(tokens):
+def parser(tokens):
+    return true
