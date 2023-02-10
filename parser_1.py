06954ccf302e465d857d080d01ea406346905720
a-cordero1 <98660749+a-cordero1@users.noreply.github.com>
2023-02-09 21:24:50 -0500

    muchas vainas pero bien

--- parser_1.py
+++ parser_1.py
@@ -12,8 +12,11 @@ direcciones1 = ['front','right','left','back']
 direcciones2 = ['right','left','around']
 card = ['north','east','west','south']
 
-#Se inicia asumiendo que se tiene una sintaxis correcta
-correcto = True
+#Diccionario donde se guardaran las funciones del PROCS
+funciones = {}
+
+#Lista donde se guardaran las variables
+variables = []
 
 def main():
     filename = 'archivoPrueba1.txt'
@@ -39,22 +42,80 @@ def syntaxError(frameinfo):
     
 def comprobarVARS(tokens: list, posLista: int, correcto: bool):
     fin = False
+    
     while correcto == True and fin == False:
+
         if tokens[posLista] == ";":
             fin = True
             posLista += 1
             break
+
         if posLista % 2 == 0:
             correcto = tokens[posLista].isalnum()
+            variables.append(tokens[posLista])
+
         if posLista % 2 == 1 and tokens[posLista] != ",":
-            correcto = False 
+            correcto = False
+
         posLista += 1
+
     return correcto, posLista
 
+def verificarParametros(tokens: list, posLista: int, correcto: bool, llaveDict: str):
+    fin = False
+    if "|" not in tokens[posLista]:
+        correcto = False
+    else:
+        x = tokens[posLista]
+        if len(tokens[posLista]) > 1:
+            if tokens[posLista][1:].isalnum():
+                funciones[llaveDict].append(tokens[posLista][1:])
+                posLista += 1
+            else:
+                correcto = False
+        else:
+            posLista += 1
+        while correcto == True and fin == False:
+            x = tokens[posLista]
+            if tokens[posLista] == ",":
+                posLista += 1
+            elif "|" not in tokens[posLista]:
+                if tokens[posLista].isalnum():
+                    funciones[llaveDict].append(tokens[posLista])
+                    posLista += 1
+                else:
+                    correcto = False
+            else:
+                fin = True
+                if len(tokens[posLista]) > 1:
+                    if tokens[posLista][:-1].isalnum():
+                        funciones[llaveDict].append(tokens[posLista][:-1])
+                        posLista += 1
+                    else:
+                        correcto = False
+    return correcto, posLista
+
+
 def comprobarPROCS(tokens: list, posLista: int, correcto: bool):
+    fin = False
+    while correcto == True and fin == False:
+        if tokens[posLista].isalnum():
+                llaveDict = tokens[posLista]
+                llaveDict = llaveDict.lower()
+                funciones[llaveDict] = []
+                posLista += 1
+                if tokens[posLista] != "[":
+                    correcto = False
+                    break
+                posLista += 1
+                correcto, posLista = verificarParametros(tokens, posLista, correcto, llaveDict)
+        else:
+            correcto = False
+        
     return correcto, posLista
 
 def inKeywords(tokens:list, keywords:list):
+
     correcto = True
     if tokens[0] != keywords[0]:
         correcto = False
@@ -74,7 +135,62 @@ def inKeywords(tokens:list, keywords:list):
         
     return correcto, cont
     
+def verificarAssingTo(tokens:list, posicion: int, correcto: bool, variables: list):
     
+    if tokens[posicion].isdigit():
+        posicion += 1
+        if tokens[posicion] == ",":
+            posicion += 1
+            for i in variables:
+                if tokens[posicion] == i:
+                    posicion += 1
+                    correcto = True
+                    break
+                else:
+                    correcto = False
+            if tokens[posicion] == ";":
+                posicion += 1
+            else:
+                correcto = False
+        else:
+            correcto = False
+    else:
+        correcto = False
+    return correcto, posicion
+
+def verificarGoTo(tokens:list, posicion: int, correcto: bool, variables: list):
+
+    inVariable = False
+    for i in variables:
+        if tokens[posicion] == i:
+            inVariable = True
+            break
+
+    if tokens[posicion].isdigit() or inVariable:
+        posicion += 1
+        if tokens[posicion] == ",":
+            posicion += 1
+            inVariable2 = False
+            for i in variables:
+                if tokens[posicion] == i:
+                    inVariable2 = True
+                    break
+            if tokens[posicion].isdigit() or inVariable2:
+                posicion += 1
+            else:
+                correcto = False
+        else:
+            correcto = False
+    else:
+        correcto = False
+
+    return correcto, posicion
+
+
+    
+
+
+
         
         
 def parser(tokens:list):
